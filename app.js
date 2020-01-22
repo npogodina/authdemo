9b4123914545da720400fed4674455c9103f7dce
Nataliya Pogodina <n.pogodina@gmail.com>
2020-01-22 10:18:31 -0800

    Created User model and configured Passport.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express               = require("express"),
+      mongoose              = require("mongoose"),
+      passport              = require("passport"),
+      bodyParser            = require("body-parser"),
+      User                  = require("./models/user"),
+      localStrategy         = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect("mongodb:/localhost:27017/auth_demo_app");
+mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
+mongoose.set('useUnifiedTopology', true);
 
 const app = express();
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "Cody Farmer",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
