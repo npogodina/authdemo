61cd26d14fe1d6e44304893ada74c23b9342d9bb
Nataliya Pogodina <n.pogodina@gmail.com>
2020-01-22 10:45:16 -0800

    Added Register route and form.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.set('useUnifiedTopology', true);
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Cody Farmer",
     resave: false,
@@ -19,10 +20,12 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({extended: true}));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// ROUTES
+// ======
 
 app.get("/", function(req, res){
     res.render("home");
@@ -31,6 +34,27 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
     res.render("secret");
 })
+
+// Auth Routes
+
+// Show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Handling user sign-up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        };
+    });
+});
 
 app.listen(3000, function(){
     console.log("Server started");
